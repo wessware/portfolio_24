e82219e4e30c748d4b7da4365744ad0a3f3745ae
wessware <stevenshisia@yahoo.com>
2024-04-19 22:29:25 +0300

    logo trial update

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,8 +3,8 @@ import nairobi from './backgrounds/nairobi.png';
 import whiteabstract from './backgrounds/white-abstract.png';
 import worldmap from './backgrounds/world-map.png';
 
-import logo from './logo/logo-black.png';
-import logotext from './logo/logo-text-black.png';
+import logo from './logo/logo.png';
+import logotext from './logo/logo_text.png';
 
 import wessware from './personal/wessware.png';
 
@@ -57,7 +57,7 @@ export {
   worldmap,
   logo,
   logotext,
-  shaq,
+  wessware,
   backend,
   ux,
   frontend,
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -108,7 +108,7 @@ const Projects = () => {
     <div className="-mt-[6rem]">
       <motion.div variants={textVariant()}>
         <p className={`${styles.sectionSubText} `}>Case Studies</p>
-        <h2 className={`${styles.sectionHeadTextLight}`}>Projects.</h2>
+        <h2 className={`${styles.sectionHeadTextLight}`}>Projects</h2>
       </motion.div>
 
       <div className="w-full flex">
