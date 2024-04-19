77b5d2adb9a2b2c1851436e98df819c617252bcd
wessware <stevenshisia@yahoo.com>
2024-04-19 22:24:46 +0300

    several changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 //mport { Link } from 'react-router-dom';
 import { styles } from '../styles';
 //import { navLinks } from '../constants';
-import { shaq, bwmap, worldmap } from '../assets';
+import { wessware, bwmap, worldmap } from '../assets';
 
 const Hero = () => {
   return (
@@ -86,8 +86,8 @@ const Hero = () => {
             className="absolute bottom-0 ml-[50vw] 
             lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
             sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
-            alt="shaquille"
+            src={wessware}
+            alt="wessware"
           />
         </div>
       </section>
