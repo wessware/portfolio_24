d63d19850b8327b4ec07473f623cccc65980a4ec
wessware <stevenshisia@yahoo.com>
2024-04-29 18:42:47 +0300

    refactors

--- src/App.jsx
+++ src/App.jsx
@@ -11,38 +11,37 @@ import {
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div className="relative z-0">
-        <div>
-          <Navbar />
-          <Hero />
-        </div>
-
-        <div className="bg-about bg-cover bg-center bg-no-repeat">
-          <About />
-        </div>
-
-        <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
-          <Tech />
-        </div>
-
-        <Projects />
-
-        <div
-          className="bg-experience bg-cover bg-center bg-no-repeat 
-            rounded-tl-[150px] rounded-br-[150px]">
-          <div
-            className="bg-experienceLight bg-cover bg-center 
-            bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
-            <Experience />
-          </div>
-        </div>
+      <BrowserRouter>
         <div className="relative z-0">
-          <Contact />
+            <div>
+              <Navbar />
+              <Hero />
+            </div>
+
+            <div className="bg-about bg-cover bg-center bg-no-repeat">
+              <About />
+            </div>
+
+            <div className="bg-tech bg-cover bg-center bg-no-repeat pb-10">
+              <Tech />
+            </div>
+
+          <Projects />
+
+          <div
+            className="bg-experience bg-cover bg-center bg-no-repeat 
+              rounded-tl-[150px] rounded-br-[150px]">
+              <div
+                className="bg-experienceLight bg-cover bg-center 
+                bg-no-repeat rounded-tl-[150px] rounded-br-[130px]">
+                <Experience />
+              </div>
+          </div>
+            <div className="relative z-0">
+              <Contact />
+            </div>
         </div>
-      </div>
-    </BrowserRouter>
+      </BrowserRouter>
   );
 };
-
 export default App;
